507474c4b27988b4ee87eba046e1c101697488b8
rakoc001 <chris_rakowski4@hotmail.com>
2025-09-19 18:00:16 +0100

    Converting App from function to Class and adding props to image link form

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -6,17 +6,34 @@ import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <ParticlesBg color='#ffffff' num={200} type='cobweb' bg={true} />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('Click')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <ParticlesBg color='#ffffff' num={200} type='cobweb' bg={true} />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
