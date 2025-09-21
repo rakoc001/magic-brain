219a61ef11896d260df913d07f37c17a88e9a6a7
rakoc001 <chris_rakowski4@hotmail.com>
2025-09-21 21:54:31 +0100

    Setup the basic clarifai API functionality

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
 import ParticlesBg from 'particles-bg'
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation' 
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '1398bafad6ee4c2cb8db75dbca8ae3fe',
+});
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = '4bb81651a1014c1f8fd16f8090d9fb26';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'rakoc001';
+  const APP_ID = 'ZTM-Smart-Brain';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+};
 
 class App extends Component {
   constructor() {
@@ -19,7 +63,21 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('Click')
+    console.log('Click');
+    this.setState({imageUrl: this.state.input});
+    // app.models.predict("face-detection", "https://samples.clarifai.com/face-det.jpg")
+    // .then(
+    //   function(response) {
+    //     console.log(response);
+    //   },
+    //   function(err) {
+    //     console.log("An error has occurred", err);
+    //   }
+    // );
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(this.state.input))
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
   }
 
   render() {
